ec13b827dcfb6cfe6844cac5b495a2d6fe8ae65f
Andy Weng <andy.weng@berkeley.edu>
2025-03-18 23:16:13 -0700

    remove project1 and project2

--- project2/src/proj2_pkg/src/proj2/converter/__init__.py
+++
@@ -1,1 +0,0 @@
-from bicycle_converter import BicycleConverter
--- project2/src/proj2_pkg/src/proj2/planners/__init__.py
+++
@@ -1,4 +0,0 @@
-from .sinusoid_planner import SinusoidPlanner
-from .rrt_planner import RRTPlanner
-from .optimization_planner import OptimizationPlanner
-from .configuration_space import BicycleConfigurationSpace
